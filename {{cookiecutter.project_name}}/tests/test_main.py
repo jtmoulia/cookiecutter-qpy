be48d3a825fe62dbbef909561c1a96cb1e51c49b
Thomas Moulia <jtmoulia@gmail.com>
2025-05-15 16:15:54 -0700

    Adjust defaults to match my usage

---
+++ {{cookiecutter.project_name}}/tests/test_main.py
@@ -0,0 +1,5 @@
+import {{cookiecutter.project_slug}}.__main__ as main
+
+
+def test_main():
+    assert main.main() is None
---
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__main__.py
@@ -0,0 +1,7 @@
+def main() -> None:
+    """Main entrypoint."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
